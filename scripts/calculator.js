2b6d674e8208ce2847771b7952580d8e37550de5
harmolipi <nikob381@gmail.com>
2021-11-27 19:41:13 -0500

    Correctly created add()

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,5 +1,5 @@
 function add(a, b) {
-  return 3;
+  return a + b;
 }
 
 module.exports = add;
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -4,3 +4,7 @@ const add = require('../scripts/calculator');
 test('1 + 2 = 3', () => {
   expect(add(1, 2)).toBe(3);
 });
+
+test('50 + 26 = 76', () => {
+  expect(add(50, 26)).toBe(76);
+});
