6aa74235c4969d279f9afa1449ae054e3df9ef65
harmolipi <nikob381@gmail.com>
2021-11-27 19:20:51 -0500

    Created another test and solution

--- tests/reverse-string.test.js
+++ tests/reverse-string.test.js
@@ -4,3 +4,7 @@ const reverseString = require('../scripts/reverse-string');
 test('reverses "hello"', () => {
   expect(reverseString('hello')).toBe('olleh');
 });
+
+test('reverses a sentence', () => {
+  expect(reverseString('my name is Harmolipi')).toBe('ipilomraH si eman ym');
+});
