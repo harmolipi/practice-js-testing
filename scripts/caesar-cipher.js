1ba13d0789bc6357a8ae8c3f70ab5f0b9a078c79
harmolipi <nikob381@gmail.com>
2021-11-28 13:52:49 -0500

    Refactored into separate function

--- scripts/caesar-cipher.js
+++ scripts/caesar-cipher.js
@@ -1,11 +1,14 @@
 function caesarCipher(str, shift) {
   return str.replace(/[a-z]/gi, (char) => {
-    let caseAdjustment =
-      char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122 ? 97 : 65;
     return String.fromCharCode(
-      ((char.charCodeAt(0) - caseAdjustment + shift) % 26) + caseAdjustment
+      ((char.charCodeAt(0) - caseAdjustment(char) + shift) % 26) +
+        caseAdjustment(char)
     );
   });
+}
+
+function caseAdjustment(char) {
+  return char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122 ? 97 : 65;
 }
 
 module.exports = caesarCipher;
