c176af44a099e3280c668746af28df6237a32bf6
harmolipi <nikob381@gmail.com>
2021-11-27 19:16:30 -0500

    Added another test

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,5 +1,10 @@
+const { expect } = require('@jest/globals');
 const capitalize = require('../scripts/capitalize');
 
 test('capitalizes "hello world"', () => {
   expect(capitalize('hello world')).toBe('Hello world');
+});
+
+test('nothing changes if already capitalized', () => {
+  expect(capitalize('Harmolipi')).toBe('Harmolipi');
 });
