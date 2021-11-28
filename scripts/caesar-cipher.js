a6579e4ac1528092dcbb9273ca46283b6a53c197
harmolipi <nikob381@gmail.com>
2021-11-28 13:51:02 -0500

    Finished caesarCipher()

--- scripts/caesar-cipher.js
+++ scripts/caesar-cipher.js
@@ -1,5 +1,11 @@
 function caesarCipher(str, shift) {
-  return 'ifmmp xpsme';
+  return str.replace(/[a-z]/gi, (char) => {
+    let caseAdjustment =
+      char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122 ? 97 : 65;
+    return String.fromCharCode(
+      ((char.charCodeAt(0) - caseAdjustment + shift) % 26) + caseAdjustment
+    );
+  });
 }
 
 module.exports = caesarCipher;
--- tests/caesar-cipher.test.js
+++ tests/caesar-cipher.test.js
@@ -3,3 +3,17 @@ const caesarCipher = require('../scripts/caesar-cipher');
 test('shifts "hello world"', () => {
   expect(caesarCipher('hello world', 1)).toBe('ifmmp xpsme');
 });
+
+test('shifts capital letters ("I am Niko")', () => {
+  expect(caesarCipher('I am Niko', 2)).toBe('K co Pkmq');
+});
+
+test(`doesn't affect punctuation ("Hi! What's your name?")`, () => {
+  expect(caesarCipher(`Hi! What's your name?`, 5)).toBe(
+    `Mn! Bmfy'x dtzw sfrj?`
+  );
+});
+
+test('properly wraps end of alphabet to beginning ("XYZ xyz")', () => {
+  expect(caesarCipher('XYZ xyz', 2)).toBe('ZAB zab');
+});
